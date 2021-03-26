7710d0e9eb00b35a8f1eb6f536ae2f670cdfb080
Stuart Klingman <stuartk@avatek.com>
2021-03-26 15:57:28 -0700

    Welcome to PicChat - Landscape Editionplus header and footer partials are available.

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
