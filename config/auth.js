1f3dc995cf31ef6774ec3bc4fac72658f20148a1
Stuart Klingman <stuartk@avatek.com>
2021-03-26 14:12:02 -0700

    Database seemed to take forever to connect, but finally showed up. 'Connected to MongoDB at sei-shard-00-01.pf3p2.mongodb.net:27017'

---
+++ config/auth.js
@@ -0,0 +1,6 @@
+module.exports = function(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    // Redirect to login if user is not already logged in
+    res.redirect('/auth/google');
+  };
+  
